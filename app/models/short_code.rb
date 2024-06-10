9c06c7b23084f324a4ae21f28839cf11d32a94d3
AmiasYaska <amiasyaska@gmail.com>
2024-06-10 22:50:49 +0300

    changes

--- app/models/short_code.rb
+++ app/models/short_code.rb
@@ -1,6 +1,17 @@
 class ShortCode
 
+    ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    BASE = ALPHABET.length
+
     def self.encode(number)
+
+        # results = ""
+        # while number > 0 do
+        #     first_calc = number % BASE
+        #     results += ALPHABET[first_calc]
+        #     results.prepend(number / BASE)
+
+        # end
         
     end
 
--- test/models/short_code_test.rb
+++ test/models/short_code_test.rb
@@ -5,10 +5,13 @@ class ShortCodeTest < ActiveSupport::TestCase
     assert_equal "0", ShortCode.encode(0)
   end
   test "encode 1" do
-    assert_equal "1", ShortCode.encode(0)
+    assert_equal "1", ShortCode.encode(1)
   end
   test "encode 11" do
-    assert_equal "b", ShortCode.encode(0)
+    assert_equal "b", ShortCode.encode(11)
+  end
+  test "encode 60" do
+    assert_equal "X", ShortCode.encode(60)
   end
 
 end
