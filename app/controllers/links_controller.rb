9364cb7326d5fc9b045bcb92f137f1ec5ff20956
AmiasYaska <amiasyaska@gmail.com>
2024-06-13 14:06:58 +0300

    changes

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -29,9 +29,7 @@ class LinksController < ApplicationController
         params.require(:link).permit(:url)
     end
 
-    def set_link
-        @link = Link.find(params[:id])
-    end
+    
 end
 
 
