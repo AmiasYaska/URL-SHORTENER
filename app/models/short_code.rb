6b4bec2707631deee604d28c80f83e476b9dcf43
AmiasYaska <amiasyaska@gmail.com>
2024-06-13 12:47:18 +0300

    changes

--- app/models/short_code.rb
+++ app/models/short_code.rb
@@ -2,7 +2,6 @@ class ShortCode
 
     ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".freeze
     BASE = ALPHABET.length
-    CHECKING = "BHU".slice()
 
     def self.encode(number)
         if number == 0
@@ -19,14 +18,21 @@ class ShortCode
     end
 
     def self.decode(string)
-        if string.length == 1
-            return ALPHABET.index(string) 
-        else
-            first_char = string.slice(0)
-            second_char = string.slice(1)
-            number_divided = (ALPHABET.index(first_char) * 62) + ALPHABET.index(second_char)
-            return number_divided
+        # if string.length == 1
+        #     return ALPHABET.index(string) 
+        # else
+        #     first_char = string.slice(0)
+        #     second_char = string.slice(1)
+        #     number_divided = (ALPHABET.index(first_char) * 62) + ALPHABET.index(second_char)
+        #     return number_divided
+        # end
+        number = 0
+        string.reverse.each_char.with_index do |char, index|
+            power = BASE**index
+            index = ALPHABET.index(char)
+            number += power * index
         end
+        number 
     end
 
 end
--- test/models/short_code_test.rb
+++ test/models/short_code_test.rb
@@ -1,40 +1,46 @@
 require "test_helper"
 
 class ShortCodeTest < ActiveSupport::TestCase
-  # test "encode 0" do
-  #   assert_equal "0", ShortCode.encode(0)
-  # end
-  # test "encode 1" do
-  #   assert_equal "1", ShortCode.encode(1)
-  # end
-  # test "encode 11" do
-  #   assert_equal "b", ShortCode.encode(11)
-  # end
-  # test "encode 62" do
-  #   assert_equal "10", ShortCode.encode(62)
-  # end
-  # test "encode 10" do
-  #   assert_equal "a", ShortCode.encode(10)
-  # end
-  # test "encode 1024" do
-  #   assert_equal "gw", ShortCode.encode(1024)
-  # end
-  # test "encode 2335" do
-  #   assert_equal "BF", ShortCode.encode(2335)
-  # end
+  test "encode 0" do
+    assert_equal "0", ShortCode.encode(0)
+  end
+  test "encode 1" do
+    assert_equal "1", ShortCode.encode(1)
+  end
+  test "encode 11" do
+    assert_equal "b", ShortCode.encode(11)
+  end
+  test "encode 62" do
+    assert_equal "10", ShortCode.encode(62)
+  end
+  test "encode 10" do
+    assert_equal "a", ShortCode.encode(10)
+  end
+  test "encode 1024" do
+    assert_equal "gw", ShortCode.encode(1024)
+  end
+  test "encode 2335" do
+    assert_equal "BF", ShortCode.encode(2335)
+  end
   test "encode 97162" do
-    assert_equal "BF", ShortCode.encode(97162)
+    assert_equal "ph8", ShortCode.encode(97162)
+  end
+  test "encode 999999" do
+    assert_equal "4c91", ShortCode.encode(999999)
   end
 
-  # test "decode b" do
-  #   assert_equal 11, ShortCode.decode("b")
-  # end
-  # test "decode BF" do
-  #   assert_equal 2335, ShortCode.decode("BF")
-  # end
-  # test "decode PH8" do
-  #   assert_equal 97162, ShortCode.decode("PH8")
-  # end
-  
+
+  test "decode b" do
+    assert_equal 11, ShortCode.decode("b")
+  end
+  test "decode BF" do
+    assert_equal 2335, ShortCode.decode("BF")
+  end
+  test "decode ph8" do
+    assert_equal 97162, ShortCode.decode("ph8")
+  end
+  test "decode 4c91" do
+    assert_equal 999999, ShortCode.decode("4c91")
+  end
 
 end
